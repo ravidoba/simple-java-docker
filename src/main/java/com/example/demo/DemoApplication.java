a9e05f9fdc8c797f4b8ac3b62100c2e4b3727110
ravidoba <ravidobariya33@gmail.com>
2025-12-04 19:58:17 +0530

    change mail application file

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,12 +3,15 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.LocalDateTime;
+
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
         System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=Testing=-=-=-=-=-=-=-=-=-=-=-");
-	}
+        System.out.println("Current Date :"+ LocalDateTime.now());
+    }
 
 }
